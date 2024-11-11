1861521ccf2efacba622c058e995020961c1fb9d
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-11 18:23:15 +0600

    header section task is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <>
-      <h1 className="font-extrabold text-6xl">Hello World</h1>
+      <Header />
     </>
   );
 }
