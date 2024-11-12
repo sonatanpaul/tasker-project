f9b1cbc7d07203adf327c9579de012fcd66371df
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-12 23:21:39 +0600

    add task feature is compelete

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import data from "../data/data";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
   const [tasks, setTasks] = useState(data);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    event.preventDefault();
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  }
 
   return (
     <>
       <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal onSave={handleAddTask} />}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask />
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskActions />
+            <TaskActions onAddClick={() => setShowAddModal(true)} />
             <TaskList tasks={tasks} />
           </div>
         </div>
